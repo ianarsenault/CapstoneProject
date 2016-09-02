89645b37472a99ed155ee9e27d507caf88beb691
ianarsenault <iaarsenault@email.neit.edu>
2016-09-02 00:14:18 -0400

    Updates: Fixed Support modal issues

--- js/script.js
+++ js/script.js
@@ -185,12 +185,6 @@ var activeScrollspy = function () {
 // ELement fire function
 var scrollfire = function () {
     var options = [
-        /* {selector: '#staggered-test', offset: 50, callback: function(el) {
-         Materialize.toast("This is our ScrollFire Demo!", 1500 );
-         } },
-         {selector: '#staggered-test', offset: 205, callback: function(el) {
-         Materialize.toast("Please continue scrolling!", 1500 );
-         } },*/
         // About card image
         {selector: '#aboutImg', offset: 300, callback: function(el) {
             Materialize.showStaggeredList($(el));
